c9ef942162dc956c61628ecb83107901c9023ce2
YeonV <trusted.yeon@gmail.com>
2022-07-12 06:36:47 +0200

    dev

--- dev.bat
+++ dev.bat
@@ -5,10 +5,11 @@ if "%~1"=="frontend" (goto frontend) else goto backend
 :frontend
 yarn start
 :backend
-%~dp0\venv\Scripts\activate.bat & ledfx -vv --offline
+cd..
+venv\Scripts\activate.bat & ledfx -vv --offline
 
 :both
-call wt -d "./backend" cmd /k "title LedFx-Backend & code . & %~dp0\venv\Scripts\activate.bat & ledfx -vv --offline"; ^
+call wt -d "./backend" cmd /k "title LedFx-Backend & code . & cd .. && venv\Scripts\activate.bat & ledfx -vv --offline"; ^
 split-pane -H -d "./frontend" cmd /k "title LedFx-Frontend & code . & call yarn start"
 
 :done
--- init.bat
+++ init.bat
@@ -1,29 +1,33 @@
 @echo off
-git clone https://github.com/YeonV/LedFx-Builds ledfx && cd ledfx
-git clone https://github.com/YeonV/LedFx-Frontend-v2 frontend
-git clone https://github.com/LedFx/LedFx backend
-
+echo Initializing LedFx:
+git clone --quiet https://github.com/YeonV/LedFx-Builds ledfx && cd ledfx
+echo Getting Frontend...
+git clone --quiet https://github.com/YeonV/LedFx-Frontend-v2 frontend 
+echo Getting Backend...
+git clone --quiet https://github.com/LedFx/LedFx backend 
+echo Creating Python Venv...
 python -m venv venv
 cd venv\Scripts
 call activate.bat
 cd ..
 cd ..
 cd backend
-pip install ..\tools\win\aubio-0.5.0a0-cp310-cp310-win_amd64.whl
-pip install aiohttp~=3.7.4
-pip install aiohttp_cors>=0.7.0
-pip install -r requirements-dev.txt
-python -m pip install --upgrade pip
-python -m pip install --upgrade wheel
-python -m pip install pywin32
+
+pip install -q ..\tools\win\aubio-0.5.0a0-cp310-cp310-win_amd64.whl
+pip install -q aiohttp~=3.7.4
+pip install -q aiohttp_cors>=0.7.0
+pip install -q -r requirements-dev.txt
+python -m pip install -q --upgrade pip
+python -m pip install -q --upgrade wheel
+python -m pip install -q  pywin32
 python ..\venv\Scripts\pywin32_postinstall.py -install
-pip install pystray
-pip install typing-extensions
+pip install -q pystray
+pip install -q typing-extensions
 python setup.py develop
 cd ..
 cp tools\win\libportaudio64bit.dll venv\Lib\site-packages\sounddevice-0.4.4-py3.10-win-amd64.egg\_sounddevice_data\portaudio-binaries
 cd frontend
-where node.exe >nul 2>&1 && echo yarn installed || call %~dp0\install-yarn.bat
+where node.exe >nul 2>&1 && echo yarn already installed || call %~dp0\ledfx\install-yarn.bat
 call yarn
 cls
-call dev.bat
+call %~dp0\ledfx\dev.bat
