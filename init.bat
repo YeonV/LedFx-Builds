1f48fd1611f2ee158bb3df2485de41e18a7d039b
YeonV <trusted.yeon@gmail.com>
2022-07-12 15:33:28 +0200

    dev

--- init.bat
+++ init.bat
@@ -1,4 +1,5 @@
 @echo off
+cls
 echo Initializing LedFx:
 git clone --quiet https://github.com/YeonV/LedFx-Builds ledfx && cd ledfx
 echo Getting Frontend...
@@ -12,22 +13,24 @@ call activate.bat
 cd ..
 cd ..
 cd backend
-
+echo Installing dependencies ...
+python -m pip install -q --upgrade pip
+python -m pip install -q --upgrade wheel
 pip install -q ..\tools\win\aubio-0.5.0a0-cp310-cp310-win_amd64.whl
 pip install -q aiohttp~=3.7.4
 pip install -q aiohttp_cors>=0.7.0
 pip install -q -r requirements-dev.txt
-python -m pip install -q --upgrade pip
-python -m pip install -q --upgrade wheel
 python -m pip install -q  pywin32
 python ..\venv\Scripts\pywin32_postinstall.py -install
 pip install -q pystray
 pip install -q typing-extensions
-python setup.py develop
+echo Setting up LedFx-core...
+python setup.py develop >nul 2>&1
 cd ..
 cp tools\win\libportaudio64bit.dll venv\Lib\site-packages\sounddevice-0.4.4-py3.10-win-amd64.egg\_sounddevice_data\portaudio-binaries
 cd frontend
+echo Installing Frontend...
 where node.exe >nul 2>&1 && echo yarn already installed || call %~dp0\ledfx\install-yarn.bat
-call yarn
-cls
+call install.bat
+@REM cls
 call %~dp0\ledfx\dev.bat
---
+++ install.bat
@@ -0,0 +1,4 @@
+@echo off
+cd ..
+cd frontend
+yarn
