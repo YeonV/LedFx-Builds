d026d892bcc9efbe0832cf97c0bdc22a7379eb3b
Yeon Vinzenz Varapragasam <trusted.yeon@gmail.com>
2022-07-12 18:34:13 +0200

    Update init.bat

--- init.bat
+++ init.bat
@@ -5,6 +5,15 @@ if exist %~dp0\ledfx\ (
   pause
   goto end 
 )
+FOR /F "tokens=2" %%F IN ('"python --version"') DO SET v=%%F
+SET pyversion=%v:~0,4%
+if %pyversion%==3.10 (
+) else if %pyversion%==3.9. (
+) else (
+  echo unsupported Python version: %pyversion%
+  pause
+  goto end
+)
 echo Initializing LedFx-Dev:
 git clone --quiet https://github.com/YeonV/LedFx-Builds ledfx && cd ledfx
 echo Getting Backend...
@@ -21,7 +30,11 @@ cd backend
 echo Installing dependencies ...
 python -m pip install -q --upgrade pip
 python -m pip install -q --upgrade wheel
-pip install -q ..\tools\win\aubio-0.5.0a0-cp310-cp310-win_amd64.whl
+if %pyversion%==3.10 (
+  pip install -q ..\tools\win\aubio-0.5.0a0-cp310-cp310-win_amd64.whl
+) else if %pyversion%==3.9. (
+  pip install -q ..\tools\win\aubio-0.5.0a0-cp39-cp39-win_amd64.whl
+)
 pip install -q aiohttp~=3.7.4
 pip install -q aiohttp_cors>=0.7.0
 pip install -q -r requirements-dev.txt
@@ -32,7 +45,12 @@ pip install -q typing-extensions
 echo Installing Backend...
 python setup.py develop >nul 2>&1
 cd ..
-copy tools\win\libportaudio64bit.dll venv\Lib\site-packages\sounddevice-0.4.4-py3.10-win-amd64.egg\_sounddevice_data\portaudio-binaries
+if %pyversion%==3.10 (
+  copy tools\win\libportaudio64bit.dll venv\Lib\site-packages\sounddevice-0.4.4-py3.10-win-amd64.egg\_sounddevice_data\portaudio-binaries
+) else if %pyversion%==3.9. (
+  copy tools\win\libportaudio64bit.dll venv\Lib\site-packages\sounddevice-0.4.4-py3.9-win-amd64.egg\_sounddevice_data\portaudio-binaries
+)
+
 cd frontend
 echo Installing Frontend...
 where yarn.exe >nul 2>&1 && echo yarn already installed || call %~dp0\ledfx\tools\win\install-yarn.bat
