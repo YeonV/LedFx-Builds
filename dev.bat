4fd915fc1d75ca2207e9aa6771cbdd0ada02f59e
YeonV <trusted.yeon@gmail.com>
2022-07-12 15:57:08 +0200

    dev

--- dev.bat
+++ dev.bat
@@ -1,4 +1,3 @@
-@echo off
 cd frontend
 if "%~1"=="" goto both
 if "%~1"=="frontend" (goto frontend) else goto backend
@@ -10,8 +9,9 @@ cd..
 venv\Scripts\activate.bat & ledfx -vv --offline
 
 :both
+cd ledfx
 call wt -d "./backend" cmd /k "title LedFx-Backend & code . & cd .. && venv\Scripts\activate.bat & ledfx -vv --offline"; ^
-split-pane -H -d "./frontend" cmd /k "title LedFx-Frontend & code . & call yarn start"
+split-pane -H -d "./frontend" cmd /k "title LedFx-Frontend & code . & yarn start"
 
 :done
 cls
