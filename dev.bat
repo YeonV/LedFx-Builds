5f922ecc22fa81cbd65ed3932d14977531603b8a
Yeon Vinzenz Varapragasam <trusted.yeon@gmail.com>
2022-07-13 06:13:35 +0200

    Update dev.bat

--- dev.bat
+++ dev.bat
@@ -14,8 +14,9 @@ goto done
 :both
 cd ..
 call wt -d "./backend" cmd /k "title LedFx-Backend & code . & cd .. && venv\Scripts\activate.bat & ledfx -vv --offline"; ^
-split-pane -H -d "./frontend" cmd /k "title LedFx-Frontend & code . & yarn start"
+split-pane -H -d "./frontend" cmd /k "title LedFx-Frontend & code . & yarn start"; ^
+split-pane -V -d "./" cmd /k "title LedFx & echo =============================== & echo     LedFx Dev Environment & echo ------------------------------- & echo Commands to start: & echo dev & echo dev frontend & echo dev backend & echo =============================== & echo Backend:  http://localhost:8888 & echo Frontend: http://localhost:3000 & echo ===============================
 
 :done
 cls
-echo LedFx-Dev-Environment startet!
+
