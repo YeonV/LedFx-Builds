98000927795df574070f26873decf160c2a5a5f9
Yeon Vinzenz Varapragasam <trusted.yeon@gmail.com>
2024-01-03 23:59:22 +0100

    Update version-bump2.ps1

--- tools/win/version-bump2.ps1
+++ tools/win/version-bump2.ps1
@@ -1,5 +1,5 @@
-(Get-Content "src\\ledfx\\consts.py") -replace "CONFIG_MICRO_VERSION ", "CONFIG_MICRO__YZ_VERSION " | Set-Content "ledfx\\consts.py"
+(Get-Content "ledfx\\consts.py") -replace "CONFIG_MICRO_VERSION ", "CONFIG_MICRO__YZ_VERSION " | Set-Content "ledfx\\consts.py"
 
-(Get-Content "src\\ledfx\\consts.py") -replace "MICRO_VERSION = \d+", "MICRO_VERSION = $args" | Set-Content "ledfx\\consts.py"
+(Get-Content "ledfx\\consts.py") -replace "MICRO_VERSION = \d+", "MICRO_VERSION = $args" | Set-Content "ledfx\\consts.py"
 
-(Get-Content "src\\ledfx\\consts.py") -replace "CONFIG_MICRO__YZ_VERSION ", "CONFIG_MICRO_VERSION " | Set-Content "ledfx\\consts.py"
+(Get-Content "ledfx\\consts.py") -replace "CONFIG_MICRO__YZ_VERSION ", "CONFIG_MICRO_VERSION " | Set-Content "ledfx\\consts.py"
