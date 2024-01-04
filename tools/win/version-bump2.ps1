6a6b7e80eab90e3725511339e18de4f3951259ab
Yeon Vinzenz Varapragasam <trusted.yeon@gmail.com>
2024-01-04 01:57:59 +0100

    Update version-bump2.ps1

--- tools/win/version-bump2.ps1
+++ tools/win/version-bump2.ps1
@@ -5,3 +5,7 @@ $versionNumber = $args[0] # get the first argument
 (Get-Content "ledfx\\consts.py") -replace "MICRO_VERSION = \d+", "MICRO_VERSION = $versionNumber" | Set-Content "ledfx\\consts.py"
 
 (Get-Content "ledfx\\consts.py") -replace "CONFIG_MICRO__YZ_VERSION ", "CONFIG_MICRO_VERSION " | Set-Content "ledfx\\consts.py"
+
+(Get-Content "pyproject.toml") -replace "version = \d+", "version = $versionNumber" | Set-Content "pyproject.toml"
+
+
